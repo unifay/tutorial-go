d1cfb6c800a6e2dfe4d43841c998b7e2b6749e9f
aapiro <tutorials@unifay.com>
2020-10-17 19:37:00 -0500

    se agrega como se manejan los string en go

---
+++ strings/aprendiendoStrings.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("String Simple")
+	fmt.Println(`
+ ESto es la primera linea
+ esta es la segunda linea, y puede contener "esto"
+	`)
+
+	fmt.Println("?")
+	fmt.Println('L')
+}
